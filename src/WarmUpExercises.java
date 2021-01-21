2e5a885d2ead8cea4d2df8684c3350c2125b1871
Dmitrii228 <200464709@student.georgianc.on.ca>
2021-01-21 15:52:41 -0500

    Compleated challege 4

--- src/WarmUpExercises.java
+++ src/WarmUpExercises.java
@@ -1,9 +1,10 @@
 public class WarmUpExercises {
     public static void main(String[] args) {
+        //Challenge 1
         for (int i=100; i>=0; i=i-5){
             System.out.println(i);
         }
-
+        //Challenge 2
            for (int i=0; i<=100; i=i+10){
 //            if(i%20==0)
 //                System.out.println(i+"java");
@@ -11,8 +12,14 @@ public class WarmUpExercises {
 //                System.out.println(i);
                System.out.println(i%20==0?i+"java":i);
         }
+        //Challenge 3
+        System.out.println(timeToSki(50));
 
 
+    }
+    //Challenge 4
+    public static boolean timeToSki(int depth){
 
+        return depth>30;
     }
 }
