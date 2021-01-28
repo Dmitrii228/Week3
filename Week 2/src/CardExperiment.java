94560c40388f93296dc892e35efdee101ab95e95
Dmitrii228 <200464709@student.georgianc.on.ca>
2021-01-28 14:18:46 -0500

    add

---
+++ Week 2/src/CardExperiment.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CardExperiment {
+    public static void main(String[] args) {
+        // Datatype  variable name      calling the constructor and passing in System.in
+        Scanner           keyboard =    new Scanner(System.in);
+        Card              aceOfSpades  = new Card("ace", "spades");
+    }
+}
