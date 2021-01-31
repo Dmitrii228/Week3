cd6b0716461b5509673d37c9b298c409a6c33260
Dmitrii228 <200464709@student.georgianc.on.ca>
2021-01-31 13:20:50 -0500

    Add final comments

--- W21-sr200464709-COMP1008-Assign1/src/TestHarness.java
+++ W21-sr200464709-COMP1008-Assign1/src/TestHarness.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class TestHarness {
     public static void main(String[] args) {
+        // Asks the user for the input
         Scanner in = new Scanner(System.in);
         System.out.print("Input your first name: ");
         String firstName = in.nextLine();
@@ -17,8 +18,10 @@ public class TestHarness {
         System.out.print("Input your student number:");
         int studentNumber= Integer.parseInt(in.nextLine());
 
+        // Initialize the object
         Student sr1 = new Student(firstName,lastName,studentNumber);
 
+        // Print out information gained form the user
         System.out.println("Your first name is:"+sr1.getFirstName());
         System.out.println("Your last name is:"+sr1.getLastName());
         System.out.println("Your student number is:"+sr1.getStudentNumber());
