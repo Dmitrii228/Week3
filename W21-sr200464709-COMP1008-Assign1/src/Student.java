6626ddd7adcde30034d3be825c62831a42fb2397
Dmitrii228 <200464709@student.georgianc.on.ca>
2021-01-31 11:48:49 -0500

    Create private instance variables and a constructor

---
+++ W21-sr200464709-COMP1008-Assign1/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int studentNumber;
+
+    public Student(String firstName, String lastName, int studentNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+    }
+}
