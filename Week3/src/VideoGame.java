8dbb0f9a6b249c0eb38161a30cc90af647468723
Dmitrii228 <200464709@student.georgianc.on.ca>
2021-02-04 15:10:58 -0500

    creat a new class

--- Week3/src/VideoGame.java
+++ Week3/src/VideoGame.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class VideoGame {
     private String name;
-    private ArrayList<String> genre;
+    private ArrayList<String> genres;
     private String ageRating;
     private String console;
 
@@ -20,40 +20,49 @@ public class VideoGame {
     }
 
     public void setName(String name) {
-        if(name.length()>=2){
-            this.name=name;
-        }
+        if (name.length() >= 2)
+            this.name = name;
         else
             throw new IllegalArgumentException("name must be at least 2 characters");
     }
 
     public ArrayList<String> getGenre() {
-        return genre;
+        return genres;
     }
 
     /**
-     * This method will return a list of all knew genres
+     * This method will return a list of all known genres
      * @return
      */
-    public static ArrayList<String> getAllGenres(){
-       ArrayList<String> genres = new ArrayList<>();
-       genres.add("Horror");
+    public static ArrayList<String> getAllGenres()
+    {
+        ArrayList<String> genres = new ArrayList<>();
+        genres.add("Horror");
         genres.add("FPS");
         genres.add("Puzzle");
         genres.add("Sports");
         genres.add("Adventure");
-        genres.add("Point and click");
+        genres.add("Point and Click");
+        genres.add("4x strategy");
         genres.add("Intro to OO");
         return genres;
-
     }
+
+    /**
+     * This will validate that the argument contains genres from a recognized list of valid
+     * genres
+     * @param genres
+     * {"Horror","Point and click"}
+     */
     public void setGenre(ArrayList<String> genres) {
         ArrayList<String> validGenres = getAllGenres();
-        for (String genre:genres) {
+        for (String genre : genres)
+        {
             if (!validGenres.contains(genre))
-                throw new IllegalArgumentException(genre + "is not valid. Valid options:"+validGenres);
+                throw new IllegalArgumentException(genre + " is not valid.  Valid options are: "
+                        +validGenres);
         }
-        this.genre = genre;
+        this.genres = genres;
     }
 
     public String getAgeRating() {
@@ -61,13 +70,12 @@ public class VideoGame {
     }
 
     public void setAgeRating(String ageRating) {
-        List<String>ageRatingValues= Arrays.asList( "E","M","18+","T","R","13+","E10+","G");
-        if (ageRatingValues.contains(ageRating)){
+        List<String> ageRatingValues = Arrays.asList("E","M","18+","T","R","13+","E10+","G");
+        if (ageRatingValues.contains(ageRating))
             this.ageRating = ageRating;
-        }
         else
-            throw new IllegalArgumentException(ageRating+" is not valid. Options are:"+ageRatingValues);
-
+            throw new IllegalArgumentException(ageRating + " is not valid.  Options are: "
+                    +ageRatingValues);
     }
 
     public String getConsole() {
